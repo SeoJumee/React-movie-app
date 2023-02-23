e746a8ed3626754e3bfa41b15c950b6d9c168313
서주미 <official.jumee@gmail.com>
2023-02-23 20:28:23 +0900

    Movie 페이지

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+export const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w1280/';
 
-const IMG_API = 'https://image.tmdb.org/t/p/w1280/';
+export default function Movie(props) {
+  const navigate = useNavigate();
 
-function Movie({ title, poster_path, vote_average }) {
+  const onClickMovieItem = () => {
+    navigate(`/movie/${props.title}`, { state: props });
+  };
+
   return (
-    <div className="movie-container">
-      <img src={IMG_API + poster_path} alt="영화포스터" />
+    <div className="movie-container" onClick={onClickMovieItem}>
+      <img src={IMG_BASE_URL + props.poster_path} alt="영화포스터" />
       <div className="movie-info">
-        <h4>{title}</h4>
-        <span>{vote_average}</span>
+        <h4>{props.title}</h4>
+        <span>{props.vote_average}</span>
       </div>
     </div>
   );
 }
-
-export default Movie;
