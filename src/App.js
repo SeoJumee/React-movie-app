9ca373d04d90448b27f39654d03df23ebc92be4e
서주미 <official.jumee@gmail.com>
2023-02-23 20:23:59 +0900

    App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import Movie from './components/Movie';
-import { dummy } from './movieDummy';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Movies from './Pages/Movies';
+import Celebrity from './Pages/Celebrity';
+import Notfound from './Pages/NotFound';
+import Tv from './Pages/Tv';
+import Header from './components/Header';
+import MovieDetail from './Pages/MovieDetail';
 
 function App() {
   return (
     <div>
-      <div className="app-container">
-        {dummy.results.map((item) => {
-          return (
-            <Movie
-              title={item.title}
-              poster_path={item.poster_path}
-              vote_average={item.vote_average}
-            />
-          );
-        })}
+      <div className="root-wrap">
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movie" element={<Movies />} />
+            <Route path="/movie/:title" element={<MovieDetail />} />
+            <Route path="/tv" element={<Tv />} />
+            <Route path="/person" element={<Celebrity />} />
+            <Route path="/*" element={<Notfound />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </div>
   );
